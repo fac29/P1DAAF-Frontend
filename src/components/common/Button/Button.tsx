8652b25369b1c5e6571f340a686d6d0328a162c9
AlexVOiceover <alex@alexvoiceover.com>
2024-05-28 16:20:57 +0100

    Added optional icons

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,20 +1,17 @@
 import React from "react"
-import { DiAppstore } from "react-icons/di";
-
 interface Props {
     name: string,
-    preIcon?: string,
-    postIcon?: string,
+    preIcon?: React.ReactNode,
+    postIcon?: React.ReactNode,
     handler: () => void
 }
 
-const Button: React.FC<Props> = ({ name, preIcon, postIcon, handler }) => {
+const Button: React.FC<Props> = ({ name, preIcon, postIcon, handler }:Props) => {
     return (
       <button onClick={handler}>
         {preIcon && <span className="pre-icon">{preIcon}</span>}
         {name}
         {postIcon && <span className="post-icon">{postIcon}</span>}
-        <DiAppstore />
       </button>
     );
   }
