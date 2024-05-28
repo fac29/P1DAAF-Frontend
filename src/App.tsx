0e9939c66c3e970377ad5f5f9197f63036416e7f
Fearg <ferskav@gmail.com>
2024-05-28 15:23:19 +0100

    Made the file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import "./App.css";
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// // Components
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
 
 function App() {
   return (
@@ -7,5 +14,39 @@ function App() {
     </>
   );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/contact">Contact</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         <Switch>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/contact">
+//             <Contact />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default App;
