eacb7f5c89f3953baaf3b9b51c86b139e8f9b242
Alex <alexperezdavies@gmail.com>
2024-05-28 16:27:29 +0100

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import "./App.css";
-// import React from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // import Dropdown from "./components/common/Dropdown/Dropdown.js";
 
@@ -11,39 +9,5 @@ function App() {
     </>
   );
 }
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/contact">Contact</Link>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         <Switch>
-//           <Route path="/about">
-//             <About />
-//           </Route>
-//           <Route path="/contact">
-//             <Contact />
-//           </Route>
-//           <Route path="/">
-//             <Home />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
 
 export default App;
