559595a85528c91122932f5e1a515d2f9d2bc4e4
Alex <alexperezdavies@gmail.com>
2024-05-29 11:13:27 +0100

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Home from "./components/pages/Home/Home";
 import Quiz from "./components/pages/Quiz/Quiz";
 import QuestionBank from "./components//pages/QuestionBank/QuestionBank";
 
-import Dropdown from "./components/common/Dropdown/Dropdown.js";
-
 function App() {
   return (
     <Router>
@@ -26,8 +24,6 @@ function App() {
             </li>
           </ul>
         </nav>
-
-        <Dropdown />
 
         <Routes>
           <Route path="/home" element={<Home />} />
