2fe3878ae4545e70a722428775eac71d095ece89
diddydeeable <diddydeeable@gmail.com>
2024-05-30 14:20:20 +0100

    cleaning up

---
+++ src/data/fetchData.ts
@@ -0,0 +1,7 @@
+//make a GET request to the API to retrieve data ( 10 questions filtered for Category and Difficulty)
+
+
+//#1 import the category and difficulty variables
+
+
+//#2 create a fetch request -> add string literals for the API
