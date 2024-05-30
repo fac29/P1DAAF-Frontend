edf09f658235ec667e6f0f539bb4fcbb4ec38a1a
Fearg <ferskav@gmail.com>
2024-05-30 12:50:23 +0100

    form displaying correctly

--- src/components/common/EditQuestionForm/EditQuestionForm.tsx
+++ src/components/common/EditQuestionForm/EditQuestionForm.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Question, Questions } from "../../../types";
 
-function EditQuestionForm() {
-  const [question, setQuestion] = useState<Question>({
+// const testQuestionObj = {
+//   id: 0,
+//   category: "",
+//   difficulty: "",
+//   question: "",
+//   options: ["", "", "", ""],
+//   answer: "",
+//   favourited: false,
+//   timestamp: new Date(),
+// };
+
+function EditQuestionForm(questionId) {
+  const [questionData, setQuestionData] = useState({
     id: 0,
     category: "",
     difficulty: "",
@@ -12,8 +23,6 @@ function EditQuestionForm() {
     favourited: false,
     timestamp: new Date(),
   });
-  const [questionValue, setQuestionValue] = useState(question?.question);
-  const [categoryValue, setCategoryValue] = useState(question?.category);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +30,7 @@ function EditQuestionForm() {
         const response = await fetch("http://localhost:3000/random/1");
 
         response.json().then((res) => {
-          setQuestion(res[0]);
+          // setQuestionData(res[0]);
         });
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -34,72 +43,150 @@ function EditQuestionForm() {
     setQuestionValue(event.target.value);
   }
 
+  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { name, type, checked, value } = event.target;
+
+    setQuestionData((prevState) => ({
+      ...prevState,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
   function handleSubmit(e: React.MouseEvent<HTMLElement>) {
     e.preventDefault();
-    console.log(questionValue);
+    console.log(questionData);
   }
 
   return (
-    <div>
-      <form>
-        {/* Render input fields based on the profileData object */}
+    <form
+      onSubmit={handleSubmit}
+      className=" text-black  p-4 bg-gray-100 rounded shadow-md "
+    >
+      <div className="grid grid-cols-2 gap-2 auto-cols-min">
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          ID:
+        </label>
+        <input
+          type="text"
+          name="id"
+          value={questionData.id}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
 
-        <div>
-          <label>Category:</label>
-          <input
-            type="text"
-            value={categoryValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Difficulty:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Question:</label>
-          <input
-            type="text"
-            value={question?.question}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Option 0:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Option 1:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Option 2:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Option 3:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-          <label>Answer:</label>
-          <input
-            type="text"
-            value={questionValue}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
+        <label className="block text-gray-700 self-end font-bold mb-2">
+          Category:
+        </label>
+        <input
+          type="text"
+          name="category"
+          value={questionData.category}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
 
-        <button type="submit" onClick={(e) => handleSubmit(e)}>
-          Save Changes
-        </button>
-      </form>
-    </div>
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Difficulty:
+        </label>
+        <input
+          type="text"
+          name="difficulty"
+          value={questionData.difficulty}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Question:
+        </label>
+        <input
+          type="text"
+          name="question"
+          value={questionData.question}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Option 0:
+        </label>
+        <input
+          type="text"
+          name="options"
+          value={questionData.options[0]}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Option 1:
+        </label>
+        <input
+          type="text"
+          name="options"
+          value={questionData.options[1]}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Option 2:
+        </label>
+        <input
+          type="text"
+          name="options"
+          value={questionData.options[2]}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Option 3:
+        </label>
+        <input
+          type="text"
+          name="options"
+          value={questionData.options[3]}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Answer:
+        </label>
+        <input
+          type="text"
+          name="answer"
+          value={questionData.answer}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Favourited:
+        </label>
+        <input
+          type="checkbox"
+          name="favourited"
+          checked={questionData.favourited}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+
+        <label className="block text-gray-700 font-bold mb-2 self-end">
+          Timestamp:
+        </label>
+        <input
+          type="date"
+          name="timestamp"
+          value={questionData.timestamp.toISOString().split("T")[0]}
+          onChange={handleInputChange}
+          className="w-full px-3 py-2 border rounded bg-white"
+        />
+      </div>
+
+      <button type="submit">Add question</button>
+    </form>
   );
 }
 
