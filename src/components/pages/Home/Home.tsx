7e5afed9a4a9fca65e3ed6ac6e52736aa0f27826
AlexVOiceover <alex@alexvoiceover.com>
2024-05-30 12:44:21 +0100

    Implemented logic dropdowns

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,36 +1,61 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from "../../common/Button/Button";
-import Dropdown from "../../common/Dropdown/Dropdown";
-//import { getCategoryFilterTypes, getDifficultyLevels } from "../../../utils/helper";
-
-
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../../common/Button/Button'
+import Dropdown from '../../common/Dropdown/Dropdown'
+import {
+	getCategoryFilterTypes,
+	getDifficultyLevels,
+} from '../../../utils/helper'
+import { CategoryFilterTypes, Difficulty } from '../../../types'
 
 function Home() {
-  // const categories = getCategoryFilterTypes();
-  // const difficulties = getDifficultyLevels();
+	const categories = getCategoryFilterTypes()
+	const difficulties = getDifficultyLevels()
 
-  const navigate = useNavigate();
-  const handler = () => {
-    navigate('/quiz');
-  };
+	const [category, setCategory] = useState<CategoryFilterTypes | undefined>(undefined)
+	const [difficulty, setDifficulty] = useState<Difficulty | undefined>(undefined)
 
-  return (
-    <div className="container mx-auto flex flex-col items-center mt-10 p-5">
-      <h1 className="text-4xl font-bold mb-4 text-white">Take a Quiz</h1>
-      <section className="mb-6 text-gray-400 text-center">
-        <p>This is the main page for the quiz.</p>
-      </section>
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
-        {/* <Dropdown name="Category" options={categories} />
-        <Dropdown name="Difficulty" options={difficulties} /> */}
-        <Dropdown  />
-        <Dropdown  />
-      </div>
-      <Button name="START QUIZ" color="orange" handler={handler} />
-    </div>
-  );
+	console.log('Category selected ' + category)
+	console.log('Difficulty selected ' + difficulty)
+
+	const navigate = useNavigate()
+	const handler = () => {
+		navigate('/quiz')
+	}
+
+	const handleCategoryChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		setCategory(event.target.value as CategoryFilterTypes)
+	}
+
+	const handleDifficultyChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		setDifficulty(event.target.value as Difficulty)
+	}
+
+	return (
+		<div className='container mx-auto flex flex-col items-center mt-10 p-5'>
+			<h1 className='text-4xl font-bold mb-4 text-white'>Take a Quiz</h1>
+			<section className='mb-6 text-gray-400 text-center'>
+				<p>This is the main page for the quiz.</p>
+			</section>
+			<div className='flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6'>
+				<Dropdown
+					name='Category'
+					contentArr={categories}
+					handleDropdown={handleCategoryChange}
+				/>
+				<Dropdown
+					name='Difficulty'
+					contentArr={difficulties}
+					handleDropdown={handleDifficultyChange}
+				/>
+			</div>
+			<Button name='START QUIZ' color='orange' handler={handler} />
+		</div>
+	)
 }
 
-export default Home;
-
+export default Home
