8fd7bfac66b28f4655568752ecbe950a30cce4a0
AlexVOiceover <alex@alexvoiceover.com>
2024-06-03 17:42:55 +0100

    Reordered vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/P1DAAF-Frontend/',
+	base: '/P1DAAF-Frontend/',
+	plugins: [react()],
 })
