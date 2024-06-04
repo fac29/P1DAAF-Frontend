8d8e3f0e80d698cde30640840e9ef79bb8b8f28c
AlexVOiceover <alex@alexvoiceover.com>
2024-06-04 13:05:26 +0100

    use 2 servers

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -7,4 +7,5 @@ export const getCategoryFilterTypes = (): string[] => {
   };
 
 // Change this to 'https://localhost:3000/' when developing!!
-  export const REACT_APP_API_URL = 'https://18.175.120.83:3000'
+  export const REACT_APP_API_URL = 'https://18.175.120.83:8443'
+  //export const REACT_APP_API_URL = 'https://localhost:8443'
