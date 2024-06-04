abd4a89d2487b868e0069d82927bbed0575099d8
Alex Perez-Davies <alexperezdavies@gmail.com>
2024-06-04 11:08:08 +0100

    Update helper.tsx

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -6,5 +6,5 @@ export const getCategoryFilterTypes = (): string[] => {
     return ['easy', 'medium', 'hard', 'all'];
   };
 
-// Change this to 'https://localhost:3000/' when developing!!
+// Change this to 'https://localhost:5173/' when developing!!
   export const REACT_APP_API_URL = 'https://18.175.120.83:3000'
