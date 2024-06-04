980213e55a47d2aa08afafa4f56d080a053bcfdf
Fearg <ferskav@gmail.com>
2024-06-04 17:43:13 +0100

    Added some basic start tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,3 +3,17 @@ describe("template spec", () => {
     cy.visit("http://localhost:5173/");
   });
 });
+
+describe("home page", () => {
+  it("the h1 contains the correct text", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("h1").contains("Take a Quiz");
+  });
+
+  it("the features on the homepage are correct", () => {
+    // cy.get(".bg-orange-500").click();
+    // cy.get(":nth-child(1) > .text-white > span").click();
+    // cy.get(".mx-2").should("exist");
+    cy.get("a").find("a").click();
+  });
+});
