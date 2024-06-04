1ab6b0b41ec559e95353715b9b501975a6919851
Alex Perez-Davies <alexperezdavies@gmail.com>
2024-06-04 11:03:56 +0100

    Update helper.tsx

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -5,5 +5,6 @@ export const getCategoryFilterTypes = (): string[] => {
   export const getDifficultyLevels = (): string[] => {
     return ['easy', 'medium', 'hard', 'all'];
   };
-  
+
+// Change this to 'https://localhost:3000/' when developing!!
   export const REACT_APP_API_URL = 'https://18.175.120.83:3000'
