db3c5e872b2f6887e40229b4356f7d1df6fcbb0c
Fearg <ferskav@gmail.com>
2024-06-05 11:46:05 +0100

    Home page functionality testing done
    Relates #49

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -10,10 +10,38 @@ describe("home page", () => {
     cy.get("h1").contains("Take a Quiz");
   });
 
-  it("the features on the homepage are correct", () => {
-    // cy.get(".bg-orange-500").click();
-    // cy.get(":nth-child(1) > .text-white > span").click();
-    // cy.get(".mx-2").should("exist");
-    cy.get("a").find("a").click();
+  describe("Home page functionality", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:5173/");
+    });
+
+    it("renders the homepage title", () => {
+      cy.get(".text-4xl").contains("Take a Quiz");
+    });
+    it("renders the Start Quiz button", () => {
+      cy.get(".bg-orange-500").should("exist");
+    });
+    it("clicks to start the quiz with dropdown selections (Geography & hard)", () => {
+      cy.get(":nth-child(1) > .bg-blue-500")
+        .select(2)
+        .invoke("val")
+        .should("eq", "Geography");
+      cy.get(":nth-child(2) > .bg-blue-500")
+        .select(3)
+        .invoke("val")
+        .should("eq", "hard");
+      cy.get(".bg-orange-500").should("exist").click();
+    });
+
+    // Working on this now to start testing the questions
+
+    // it("clicks to answer each question", () => {
+    //   cy.get('[data-value="Rockies"]').should("exist").click();
+    //   // cy.get(":nth-child(2) > .bg-blue-500")
+    //   //   .select(3)
+    //   //   .invoke("val")
+    //   //   .should("eq", "hard");
+    //   // cy.get(".bg-orange-500").should("exist").click();
+    // });
   });
 });
