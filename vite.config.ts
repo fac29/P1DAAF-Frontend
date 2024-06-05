8bef84d9d6843fd783998ddc37d76cc090ed794d
Alex <alexperezdavies@gmail.com>
2024-06-05 16:57:10 +0100

    add server config so works on my local machine

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	base: './',
 	plugins: [react()],
+	server: {
+    host: '0.0.0.0', // Change this to a valid IP address if needed
+    port: 5173, // Optional otherwise your app will start on default port
+  },
 })
