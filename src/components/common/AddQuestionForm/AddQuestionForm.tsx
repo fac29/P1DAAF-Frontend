88572350dccd2eb8cacd8c3c7486bfc7159f816f
AlexVOiceover <alex@alexvoiceover.com>
2024-06-05 12:29:11 +0100

    Answers from dropboxes

--- src/components/common/AddQuestionForm/AddQuestionForm.tsx
+++ src/components/common/AddQuestionForm/AddQuestionForm.tsx
@@ -1,180 +1,180 @@
-import React, { useState } from "react";
-import { AddQuestion } from "../../../types";
+import React, { useState } from 'react'
+import { AddQuestion } from '../../../types'
 
-import Dropdown from "../Dropdown/Dropdown";
+import Dropdown from '../Dropdown/Dropdown'
 
 import {
-  getCategoryFilterTypes,
-  getDifficultyLevels,
-} from "../../../utils/helper";
+	getCategoryFilterTypes,
+	getDifficultyLevels,
+} from '../../../utils/helper'
 
 function AddQuestionForm() {
-  const [addQuestion, setAddQuestion] = useState<AddQuestion>({
-    id: "",
-    category: "",
-    difficulty: "",
-    question: "",
-    options: ["", "", "", ""],
-    answer: "",
-    favourited: false,
-    timestamp: new Date(),
-  });
+	const [addQuestion, setAddQuestion] = useState<AddQuestion>({
+		id: '',
+		category: '',
+		difficulty: '',
+		question: '',
+		options: ['', '', '', ''],
+		answer: '',
+		favourited: false,
+		timestamp: new Date(),
+	})
 
-  const categories = getCategoryFilterTypes();
-  const difficulties = getDifficultyLevels();
+	const categories = getCategoryFilterTypes()
+	const difficulties = getDifficultyLevels()
 
-  const apiURL = import.meta.env.VITE_API_URL;
+	const apiURL = import.meta.env.VITE_API_URL
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
 
-    const newId = new Date().valueOf();
-    const newTimestamp = new Date();
-    setAddQuestion((prevState) => {
-      const updatedQuestion = {
-        ...prevState,
-        id: newId,
-        timestamp: newTimestamp,
-      };
+		const newId = new Date().valueOf()
+		const newTimestamp = new Date()
+		setAddQuestion((prevState) => {
+			const updatedQuestion = {
+				...prevState,
+				id: newId,
+				timestamp: newTimestamp,
+			}
 
-      // Return the new state
-      return updatedQuestion;
-    });
+			// Return the new state
+			return updatedQuestion
+		})
 
-    // Using the updatedQuestion for the fetch request
-    const updatedQuestion = {
-      ...addQuestion,
-      id: newId,
-      timestamp: newTimestamp,
-    };
+		// Using the updatedQuestion for the fetch request
+		const updatedQuestion = {
+			...addQuestion,
+			id: newId,
+			timestamp: newTimestamp,
+		}
 
-    fetch(`${apiURL}/create-question`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedQuestion),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response from the backend
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error("Error:", error);
-      });
-  };
+		fetch(`${apiURL}/create-question`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updatedQuestion),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				// Handle the response from the backend
+				console.log(data)
+			})
+			.catch((error) => {
+				// Handle any errors
+				console.error('Error:', error)
+			})
+	}
 
-  const handleInputChange = (
-    event:
-      | React.FormEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const { name, type, checked, value } = event.target as HTMLInputElement;
-    setAddQuestion((prevState) => {
-      if (type === "checkbox") {
-        return {
-          ...prevState,
-          [name]: checked,
-        };
-      } else if (name.startsWith("option")) {
-        const index = parseInt(name.split(" ")[1], 10) - 1;
-        const newOptions = [...prevState.options];
-        newOptions[index] = value;
-        return {
-          ...prevState,
-          options: newOptions,
-        };
-      } else {
-        return {
-          ...prevState,
-          [name]: value,
-        };
-      }
-    });
-  };
+	const handleInputChange = (
+		event:
+			| React.FormEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLSelectElement>
+	) => {
+		const { name, type, checked, value } = event.target as HTMLInputElement
+		setAddQuestion((prevState) => {
+			if (type === 'checkbox') {
+				return {
+					...prevState,
+					[name]: checked,
+				}
+			} else if (name.startsWith('option')) {
+				const index = parseInt(name.split(' ')[1], 10) - 1
+				const newOptions = [...prevState.options]
+				newOptions[index] = value
+				return {
+					...prevState,
+					options: newOptions,
+				}
+			} else {
+				return {
+					...prevState,
+					[name]: value,
+				}
+			}
+		})
+	}
 
-  return (
-    <form
-      onSubmit={handleSubmit}
-      className=" text-black  p-4 bg-gray-100 rounded shadow-md "
-    >
-      <div className="grid grid-cols-2 gap-2 auto-cols-min">
-        <label className="block text-gray-700 self-end font-bold mb-2">
-          Category:
-        </label>
-        <Dropdown
-          name="category"
-          contentArr={categories}
-          handleDropdown={handleInputChange}
-        />
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className=' text-black  p-4 bg-gray-100 rounded shadow-md '
+		>
+			<div className='grid grid-cols-2 gap-2 auto-cols-min'>
+				<label className='block text-gray-700 self-end font-bold mb-2'>
+					Category:
+				</label>
+				<Dropdown
+					name='category'
+					contentArr={categories}
+					handleDropdown={handleInputChange}
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Difficulty:
-        </label>
-        <Dropdown
-          name="difficulty"
-          contentArr={difficulties}
-          handleDropdown={handleInputChange}
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Difficulty:
+				</label>
+				<Dropdown
+					name='difficulty'
+					contentArr={difficulties}
+					handleDropdown={handleInputChange}
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Question:
-        </label>
-        <input
-          type="text"
-          name="question"
-          value={addQuestion.question}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Question:
+				</label>
+				<input
+					type='text'
+					name='question'
+					value={addQuestion.question}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Option 1:
-        </label>
-        <input
-          type="text"
-          name="option 1"
-          value={addQuestion.options[0]}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Option 1:
+				</label>
+				<input
+					type='text'
+					name='option 1'
+					value={addQuestion.options[0]}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Option 2:
-        </label>
-        <input
-          type="text"
-          name="option 2"
-          value={addQuestion.options[1]}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Option 2:
+				</label>
+				<input
+					type='text'
+					name='option 2'
+					value={addQuestion.options[1]}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Option 3:
-        </label>
-        <input
-          type="text"
-          name="option 3"
-          value={addQuestion.options[2]}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Option 3:
+				</label>
+				<input
+					type='text'
+					name='option 3'
+					value={addQuestion.options[2]}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Option 4:
-        </label>
-        <input
-          type="text"
-          name="option 4"
-          value={addQuestion.options[3]}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Option 4:
+				</label>
+				<input
+					type='text'
+					name='option 4'
+					value={addQuestion.options[3]}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
+				{/* <label className="block text-gray-700 font-bold mb-2 self-end">
           Answer:
         </label>
         <input
@@ -183,23 +183,32 @@ function AddQuestionForm() {
           value={addQuestion.answer}
           onChange={handleInputChange}
           className="w-full px-3 py-2 border rounded bg-white"
-        />
+        /> */}
+				<label className='block text-gray-700 self-end font-bold mb-2'>
+					Answer:
+				</label>
+				<Dropdown
+					name='answer'
+					contentArr={addQuestion.options}
+					handleDropdown={handleInputChange}
+				//	defaultValue={addQuestion.answer}
+				/>
 
-        <label className="block text-gray-700 font-bold mb-2 self-end">
-          Favourited:
-        </label>
-        <input
-          type="checkbox"
-          name="favourited"
-          checked={addQuestion.favourited}
-          onChange={handleInputChange}
-          className="w-full px-3 py-2 border rounded bg-white"
-        />
-      </div>
+				<label className='block text-gray-700 font-bold mb-2 self-end'>
+					Favourited:
+				</label>
+				<input
+					type='checkbox'
+					name='favourited'
+					checked={addQuestion.favourited}
+					onChange={handleInputChange}
+					className='w-full px-3 py-2 border rounded bg-white'
+				/>
+			</div>
 
-      <button type="submit">Add question</button>
-    </form>
-  );
+			<button type='submit'>Add question</button>
+		</form>
+	)
 }
 
-export default AddQuestionForm;
+export default AddQuestionForm
--- src/components/common/Dropdown/Dropdown.tsx
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -1,33 +1,32 @@
 interface Props {
-  name: string;
-  contentArr: Array<string>;
-  defaultValue?: string;
-  handleDropdown: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+	name: string
+	contentArr: Array<string>
+	defaultValue?: string
+	handleDropdown: (event: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
 function Dropdown({ name, contentArr, defaultValue, handleDropdown }: Props) {
-  return (
-    <div className="flex justify-evenly">
-      <select
-        name={name}
-        onChange={(e) => handleDropdown(e)}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center"
-      >
-        {defaultValue ? (
-          <option value="">{defaultValue}</option>
-        ) : (
-          <option value="">Please choose {name}</option>
-        )}
-        {contentArr?.map((category, index) => {
-          return (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          );
-        })}
-      </select>
-    </div>
-  );
+	return (
+		<div className='flex justify-evenly'>
+			<select
+				name={name}
+				onChange={(e) => handleDropdown(e)}
+				value={defaultValue}
+				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center'
+			>
+				<option value=''>{defaultValue ? 'None' : `Please choose ${name}`}</option>
+				{contentArr
+					//.filter((option) => option !== defaultValue)
+					.map((option, index) => {
+						return (
+							<option key={index} value={option}>
+								{option}
+							</option>
+						)
+					})}
+			</select>
+		</div>
+	)
 }
 
-export default Dropdown;
+export default Dropdown
