35f534af9d84d11429a11c8e0bbec60683147f20
Fearg <ferskav@gmail.com>
2024-06-05 12:38:13 +0100

    Tests the homepage and quiz functionality
    Relates #49

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -21,27 +21,47 @@ describe("home page", () => {
     it("renders the Start Quiz button", () => {
       cy.get(".bg-orange-500").should("exist");
     });
-    it("clicks to start the quiz with dropdown selections (Geography & hard)", () => {
+    it("clicks to start the quiz with dropdown selections (Music & easy)", () => {
       cy.get(":nth-child(1) > .bg-blue-500")
-        .select(2)
+        .select(6)
         .invoke("val")
-        .should("eq", "Geography");
+        .should("eq", "Music");
       cy.get(":nth-child(2) > .bg-blue-500")
-        .select(3)
+        .select(1)
         .invoke("val")
-        .should("eq", "hard");
+        .should("eq", "easy");
       cy.get(".bg-orange-500").should("exist").click();
     });
+  });
+});
 
-    // Working on this now to start testing the questions
+describe("home page", () => {
+  describe("Home page functionality", () => {
+    it("starts the quiz", () => {
+      cy.visit("http://localhost:5173/");
 
-    // it("clicks to answer each question", () => {
-    //   cy.get('[data-value="Rockies"]').should("exist").click();
-    //   // cy.get(":nth-child(2) > .bg-blue-500")
-    //   //   .select(3)
-    //   //   .invoke("val")
-    //   //   .should("eq", "hard");
-    //   // cy.get(".bg-orange-500").should("exist").click();
-    // });
+      cy.get(".text-4xl").contains("Take a Quiz");
+
+      cy.get(".bg-orange-500").should("exist");
+
+      cy.get(":nth-child(1) > .bg-blue-500")
+        .select(6)
+        .invoke("val")
+        .should("eq", "Music");
+      cy.get(":nth-child(2) > .bg-blue-500")
+        .select(1)
+        .invoke("val")
+        .should("eq", "easy");
+      cy.get(".bg-orange-500").should("exist").click();
+
+      cy.get('[data-value="The Grammy Award"]').should("be.visible").click();
+      cy.get(".mx-2").should("be.visible").click();
+
+      cy.get('[data-value="Ludwig van Beethoven"]').should("exist").click();
+      cy.get(".mx-2").should("exist").click();
+
+      cy.get(".top-2").should("exist").click();
+      cy.get(".mx-2").should("exist").click();
+    });
   });
 });
